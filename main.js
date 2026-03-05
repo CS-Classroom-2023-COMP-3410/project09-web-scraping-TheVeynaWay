8f6c2b57e5bc1a987a9f233223e1784ab5f9242e
Garrett Veyna <garrett.veyna@du.edu>
2026-03-05 04:06:09 +0000

    Question 1 and 3 working, 2 isn't

---
+++ main.js
@@ -0,0 +1,64 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs-extra');
+
+axios.get('https://bulletin.du.edu/undergraduate/coursedescriptions/comp/').then(async response => {
+    const $ = cheerio.load(response.data);
+    const courses = [];
+
+    $('.courseblocktitle').each((i, el) => {
+        const text = $(el).text().trim();
+
+        const beforeCredits = text.split('(')[0].trim();
+        const parts = beforeCredits.split(/\s+/);
+
+        const number = parseInt(parts[1]);
+        if (number < 3000) return;
+
+        const title = parts.slice(2).join(' ').trim();
+
+        const desc = $(el).next('.courseblockdesc').text();
+        if (desc.includes('Prerequisite')) return;
+
+        courses.push({
+            course: `COMP-${number}`,
+            title: title
+        });
+    });
+
+    await fs.ensureDir('results');
+    await fs.writeJson('results/bulletin.json', { courses }, { spaces: 4 });
+});
+
+axios.get('https://denverpioneers.com/index.aspx').then(response => {
+    const $ = cheerio.load(response.data);
+    const events = [];
+
+    //Code wasn't working, deleted it, need to fix but ran out of time
+
+    fs.writeFileSync("results/athletic_events.json", JSON.stringify({ events }, null, 4));
+    console.log("Scraping complete!");
+});
+
+axios.get('https://www.du.edu/calendar').then(response => {
+    const $ = cheerio.load(response.data);
+    const events = [];
+
+    $("a.event-card").each((i, el) => {
+
+        const title = $(el).find("h3").text().trim();
+        const date = $(el).find("p").first().text().trim();
+
+        const timeText = $(el).find("span.icon-du-clock").parent().text().trim();
+
+        let event = { title: title, date: date };
+
+        if (timeText) {
+            event.time = timeText;
+        }
+
+        events.push(event);
+    });
+
+    fs.writeFileSync("results/calendar_events.json", JSON.stringify({ events: events }, null, 4));
+});
